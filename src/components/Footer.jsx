75b0eef63c7f4bddef1e1cba4ff80bf5f0276fc0
Mpdanish <danishmaliyekkal786@gmail.com>
2024-12-13 18:42:57 +0530

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,20 @@ const Footer = () => {
     <Section crosses className="!px-0 !py-10">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <p className="caption text-n-4 lg:block">
-          © {new Date().getFullYear()}. All rights reserved. Copyrighted to Kode & Klick LLP.
+          © {new Date().getFullYear()}. All rights reserved. Copyrighted to Kode
+          & Klick LLP.
         </p>
-
-       
+        <p className="caption text-n-4 lg:block">
+          Developed by{" "}
+          <a
+            href="https://www.zeecodesolutions.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            Zeecode Solutions
+          </a>
+        </p>
       </div>
     </Section>
   );
